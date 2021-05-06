28f7fcff612acdcd4c1018b1d0305922f48deba2
joearrowsmith <joearrowsmith98@gmail.com>
2021-05-06 14:02:54 +0100

    minor edit

--- create_tf_dataset.py
+++ create_tf_dataset.py
@@ -20,10 +20,5 @@ def create_tfrecords(folder="train", use_2d=False, crop_size=None, mid_folders="
                        crop_size=crop_size)
     
 if __name__ == "__main__":
-<<<<<<< HEAD
-    # create_tfrecords("train", mid_folders='/mnt')
-    create_tfrecords("valid", mid_folders='/mnt')
-=======
     create_tfrecords("train", mid_folders='/mnt', use_2d=True)
     create_tfrecords("valid", mid_folders='/mnt', use_2d=True)
->>>>>>> 264fb92740314ab69e0bf0bd1c057ebe22ff4ac5
